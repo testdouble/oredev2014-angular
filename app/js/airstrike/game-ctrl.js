e81c8a3d11a34f989f6113f1661938234c56dfbd
Zach <briggszj@gmail.com>
2014-11-02 16:31:31 -0600

    Finish airstrike video 2

--- app/js/airstrike/game-ctrl.js
+++ app/js/airstrike/game-ctrl.js
@@ -1,6 +1,22 @@
 (function () {
     "use strict";
 
-    angular.module("app").controller("gameCtrl", function () {
+    angular.module("app").controller("gameCtrl", function ($scope) {
+        var lastShot = null;
+        var enemyShips = ["A1", "B1", "C2", "C3", "C4"];
+
+        $scope.status = function () {
+            if (lastShot === null) {
+                return "You may fire when ready.";
+            } if (enemyShips.indexOf(lastShot) > -1) {
+                return "HIT!";
+            } else {
+                return "MISS!";
+            }
+        };
+
+        $scope.fire = function (shot) {
+            lastShot = shot;
+        };
     });
 }());
